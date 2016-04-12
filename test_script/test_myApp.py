f2348f13f444dac36fd586eb66610e3cdb35ae78
0300002580 <Mitsuaki.Maruyama@jp.sony.com>
2016-04-12 11:53:40 +0900

    もいちどコミット

--- test_script/test_myApp.py
+++ test_script/test_myApp.py
@@ -4,5 +4,7 @@ class TestMyApp(TestCase):
     def test_get_name(self):
         self.assertTrue("pass")
 
+        self.assertTrue("pass")
+
     def test_set_name(self):
         self.assertTrue("pass")
